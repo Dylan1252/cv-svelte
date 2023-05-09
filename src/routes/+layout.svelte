7b2fe30bda9bd827e2dcaa53ac0d9699d7df7ca6
Dylan1252 <124692275+Dylan1252@users.noreply.github.com>
2023-05-09 08:41:22 +0000

    yes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,30 @@
+<svelte:head>
+    
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" >
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500&family=Peralta&display=swap"
+rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://kit.fontawesome.com/d0c488930b.js" crossorigin="anonymous"></script>
+</svelte:head>
 
-<div class = " parent">
-    <div class="sidebar"/>
-    <div class="content">
+
+
 <slot/>
-</div>
-</div>
+
 
 <style>
 
 :global(html,body) {
     width: 100%;
     height: 100%;
+    background: #fffde7;
 }
 
 :global(*){
     margin:0;
     padding:0;
+    font-family: "Open sans";
 }
 
 
@@ -30,7 +39,7 @@
 
 .sidebar {
 
-    background-color: blue;
+    background-color: cyan;
     width: 200px;
     height : 100%;
 
